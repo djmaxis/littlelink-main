87886701940938c431a2fb4db0dd4496892bd956
djmaxis <126213323+djmaxis@users.noreply.github.com>
2023-05-12 22:19:50 -0400

    Add files via upload

--- apiReservas/js/form.js
+++ apiReservas/js/form.js
@@ -2,7 +2,7 @@ document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
   //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  const telefono = "+18295463303
+  const telefono = "+18295463303";
 
   const cliente = document.querySelector("#cliente").value;
   const fecha = document.querySelector("#fecha").value;
@@ -15,17 +15,16 @@ document.querySelector("#submit").addEventListener("click", e => {
   resp.classList.remove("send");
 
   const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-	
-		*Saludos!*%0A%0A
+		
+		*Buenas*%0A%0A
 		*Mi nombre es:*%0A
 		${cliente}%0A
-		*Quiero hacer una reserva en esta fecha:*%0A
-		${fecha }$ a las ${ hora}%0A
-		*Mi paseador:*%0A
+		*Quiero reservar para: el:*%0A
+		${fecha} a las ${hora}%0A
+		*Empleado de preferencia*%0A
 		${empleado}%0A
-		*Con este tipo de servicio*%0A
-		${servicio}%0A
-        *Gracias*%0A` ;
+		*¿Cuál es el servicio que se desea realizar?*%0A
+		${servicio}`;
 
   if (cliente === "" || fecha === "" || hora === "") {
     resp.classList.add("fail");
@@ -34,7 +33,7 @@ document.querySelector("#submit").addEventListener("click", e => {
   }
   resp.classList.remove("fail");
   resp.classList.add("send");
-  resp.innerHTML = `Se ha enviado tu reserva, Gracias ${cliente}`;
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
 
   window.open(url);
 });
