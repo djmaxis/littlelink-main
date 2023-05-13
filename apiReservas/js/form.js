69141bd6e44053772bdbb0a15b12d8fd85b0426f
djmaxis <126213323+djmaxis@users.noreply.github.com>
2023-05-13 00:19:57 -0400

    Add files via upload

--- apiReservas/js/form.js
+++ apiReservas/js/form.js
@@ -6,6 +6,11 @@ document.querySelector("#submit").addEventListener("click", e => {
 
   const cliente = document.querySelector("#cliente").value;
   const perro = document.querySelector("#perro").value;
+  const gperro = document.querySelector("#gperro").value;
+  const tperro = document.querySelector("#tperro").value;
+  const alperro = document.querySelector("#alperro").value;
+  const cperro = document.querySelector("#cperro").value;
+  const sperro = document.querySelector("#sperro").value;
   const fecha = document.querySelector("#fecha").value;
   const hora = document.querySelector("#hora").value;
   const empleado = document.querySelector("#empleado").value;
@@ -20,15 +25,15 @@ document.querySelector("#submit").addEventListener("click", e => {
 		*Buenas*%0A%0A
 		*Mi nombre es:*%0A
 		${cliente}%0A
+        *Yo eleji este servicio:*%0A
+		${servicio}%0A%0A
 		*Quiero reservar para el:*%0A
 		${fecha} a las ${hora}%0A
 		*Mi paseador:*%0A
 		${empleado}%0A
         *Estara ciudando a:%0A
         ${perro}%0A
-		*Yo eleji este servicio:*%0A
-		${servicio}%0A%0A
-         Gracias!`;
+         Recuerden que ${perro} de genero ${gperro} y tamano ${tperro}, es alergic@ a ${alperro} y tiene un comportamiento ${cperro}, ademas ${perro} tiene un ${sperro}`;
 
   if (cliente === "" || fecha === "" || hora === "") {
     resp.classList.add("fail");
